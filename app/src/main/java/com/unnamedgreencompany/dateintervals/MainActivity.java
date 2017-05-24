9c3ded831934ad8b706e08d5be24ad148c943b1f
Nivek <kevin_ruoyu_zheng@hotmail.com>
2017-05-24 18:38:53 -0400

    Added input validation and error messages

--- app/src/main/java/com/unnamedgreencompany/dateintervals/MainActivity.java
+++ app/src/main/java/com/unnamedgreencompany/dateintervals/MainActivity.java
@@ -1,11 +1,14 @@
 package com.unnamedgreencompany.dateintervals;
 
+import android.graphics.Color;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -20,11 +23,18 @@ public class MainActivity extends FragmentActivity
     private EditText startTimeEditor;
     private EditText endDateEditor;
     private EditText endTimeEditor;
+    private EditText numIntervalsEditor;
+
+    private int minIntervals;
+    private int maxIntervals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        minIntervals = Integer.parseInt(getString(R.string.interval_min));
+        maxIntervals = Integer.parseInt(getString(R.string.interval_max));
 
         dateFormat = DateFormat.getDateFormat(this);
         timeFormat = DateFormat.getTimeFormat(this);
@@ -45,6 +55,8 @@ public class MainActivity extends FragmentActivity
         endDateEditor.setText(dateFormat.format(new Date(future.getTime())));
         endTimeEditor = (EditText)findViewById(R.id.endTimeInput);
         endTimeEditor.setText(timeFormat.format(new Date(future.getTime())));
+
+        numIntervalsEditor = (EditText)findViewById(R.id.numIntervalsInput);
     }
 
     public void showDatePickerDialog(View v) {
@@ -71,6 +83,50 @@ public class MainActivity extends FragmentActivity
         Calendar c = Calendar.getInstance();
         c.set(year, month, day);
         dateEditor.setText(dateFormat.format(c.getTime()));
+    }
+
+    private Calendar mergeDateAndTime(Date date, Date time) {
+        Calendar timeCal = Calendar.getInstance();
+        timeCal.setTime(time);
+        Calendar dateCal = Calendar.getInstance();
+        dateCal.setTime(date);
+        dateCal.set(Calendar.HOUR_OF_DAY, timeCal.get(Calendar.HOUR_OF_DAY));
+        dateCal.set(Calendar.MINUTE, timeCal.get(Calendar.MINUTE));
+        dateCal.set(Calendar.SECOND, 0);
+        dateCal.set(Calendar.MILLISECOND, 0);
+        return dateCal;
+    }
+
+    public void submitInputs(View v) {
+        int numIntervals = Integer.parseInt(numIntervalsEditor.getText().toString());
+        if (numIntervals < minIntervals || numIntervals > maxIntervals) {
+            showError(R.string.interval_count_bounds);
+            return;
+        }
+        Calendar startPoint, endPoint;
+        try {
+            Date startDate = dateFormat.parse(startDateEditor.getText().toString());
+            Date startTime = timeFormat.parse(startTimeEditor.getText().toString());
+            startPoint = mergeDateAndTime(startDate, startTime);
+            Date endDate = dateFormat.parse(endDateEditor.getText().toString());
+            Date endTime = timeFormat.parse(endTimeEditor.getText().toString());
+            endPoint = mergeDateAndTime(endDate, endTime);
+        }
+        catch (Exception e) {
+            showError(R.string.invalid_dates);
+            return;
+        }
+        if (endPoint.before(startPoint)) {
+            showError(R.string.end_before_start);
+            return;
+        }
+    }
+
+    private void showError(int messageId) {
+        Toast toast = Toast.makeText(getApplicationContext(), messageId, Toast.LENGTH_LONG);
+        TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
+        toastMessage.setBackgroundColor(Color.RED);
+        toast.show();
     }
 
     @Override
