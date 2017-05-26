fee928a875587df2a237bb3f7a9aa213a93c502d
Nivek <kevin_ruoyu_zheng@hotmail.com>
2017-05-26 19:16:25 -0400

    Added functionality to write results to local file as csv in Downloads folder

--- app/src/main/java/com/unnamedgreencompany/dateintervals/ResultsActivity.java
+++ app/src/main/java/com/unnamedgreencompany/dateintervals/ResultsActivity.java
@@ -2,21 +2,33 @@ package com.unnamedgreencompany.dateintervals;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.icu.text.DateFormat;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class ResultsActivity extends AppCompatActivity implements ResultsFragment.OnFragmentInteractionListener {
+
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     private String[] results;
     private int displayThreshold;
@@ -84,7 +96,52 @@ public class ResultsActivity extends AppCompatActivity implements ResultsFragmen
         }
     }
 
+    public void downloadResults(View v) {
+        try {
+            //First we need to request permission to write externally, mandatory for Android M and later
+            if (!checkPermission()) {
+                requestPermission();
+            }
+
+            String filename = String.format("Result-%s.csv", DateFormat.getDateTimeInstance().format(new Date()));
+            File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+            File file = new File(dir, filename);
+            PrintWriter writer = new PrintWriter(new FileWriter(file));
+            for (int i = 0; i < results.length; i++) {
+                writer.println(String.format(Locale.getDefault(), "%d,%s", i, results[i]));
+            }
+            writer.close();
+            showSuccess(String.format("%s %s", getString(R.string.file_save_success), file));
+        }
+        catch (Exception e) {
+            showError(String.format("%s %s", getString(R.string.file_save_failure), e.getMessage()));
+        }
+    }
+
+    private void showError(String message) {
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
+        toastMessage.setBackgroundColor(Color.RED);
+        toast.show();
+    }
+
+    private void showSuccess(String message) {
+        Toast toast = Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG);
+        TextView toastMessage = (TextView) toast.getView().findViewById(android.R.id.message);
+        toastMessage.setBackgroundColor(Color.BLUE);
+        toast.show();
+    }
+
     @Override
     public void onFragmentInteraction() {
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(ResultsActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        return result == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(ResultsActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
     }
 }
